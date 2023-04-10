8eaa6899e89ba34e7d2f1c59a6db5951fd139aba
Perry Bhandal <git@perryb.ca>
2023-04-09 18:12:51 -0700

    Revert "Fixed task removal from array."
    This reverts commit b1e4a66409bc47440ae6677ddcf0ff3da0f83550.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,10 +23,10 @@ class DashboardController < ApplicationController
   def getHistoricTasks
     taskList = []
 
-    allTasks = ActiveTask.all.order('start_time DESC').select(:task_name)
+    allTasks = ActiveTask.all.order('start_time DESC').select(:task_name).uniq()
     allTasks.each do |task|
       if PremadeTask.where(:name => task.task_name).count == 0 and !(taskList.include?(task))
-        taskList.delete(task)
+        taskList.pop(task)
         taskList.push(task)
       end
     end
